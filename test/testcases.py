e15bc9e497365c89c6f3fe70b4b864b7c3e29ed1
hylom <hylom@users.sourceforge.jp>
2015-11-11 19:37:40 +0900

    fix: test/testcases.py can execute in outside of test directory

--- test/testcases.py
+++ test/testcases.py
@@ -1,18 +1,23 @@
-
+import os.path
 import unittest
 
 import indexer
 
+
 class IndexerTestCase(unittest.TestCase):
     def setUp(self):
+	self.testdir = os.path.dirname(__file__)
         self.testdata = ['zip/test_l.zip', 'zip/test_p.zip']
+
+    def _get_rpath(self, fpath):
+	return os.path.join(self.testdir, fpath)
 
     def test_extract_thumbnail(self):
         """test extract_metadata.extract_thumbnail()"""
-        thumb = indexer.extract_metadata.extract_thumbnail('zip/test_l.zip')
+        thumb = indexer.extract_metadata.extract_thumbnail(self._get_rpath('zip/test_l.zip'))
         self.assertEqual(thumb.filetype, '.jpg')
 
-        fp = open('image/test_l/001.jpg', 'r')
+        fp = open(self._get_rpath('image/test_l/001.jpg'), 'r')
         img = fp.read()
         fp.close()
         self.assertEqual(thumb.bindata, img)
